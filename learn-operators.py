70580e82b229b40ffff2efa63a40f6812df9741e
Tyler S-C <tscizzle@gmail.com>
2019-04-28 00:59:02 -0400

    commented about the greater than problem needing 1 node

--- learn-operators.py
+++ learn-operators.py
@@ -32,6 +32,11 @@ def main():
     training_answers = greaterthan_answers[:TRAINING_CUTOFF]
     test_answers = greaterthan_answers[TRAINING_CUTOFF:]
 
+    # the neural net that solves the "greater than" problem needs only 1 node in
+    # the first layer, because the problem of comparing binary numbers is simply
+    # taking a linear combination of the bits
+    # (1*A[0] + 2*A[1] + ... - 1*B[0] - 2*b[1] - ...) and comparing to 0, but
+    # this is precisely what a single node in a neural net can do!
     model = keras.Sequential([
         keras.layers.Dense(1, activation=tf.nn.relu),
         keras.layers.Dense(2, activation=tf.nn.softmax)
